ba52ae00ef24d1acc472e2d05331d29375959633
Neru <84850523+thucnee511@users.noreply.github.com>
2023-06-06 13:52:46 +0000

    update registration fuction

--- src/controlers/ProgramRegister.java
+++ src/controlers/ProgramRegister.java
@@ -8,6 +8,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import manager.ProgramManager;
 import manager.StudentManager;
@@ -42,30 +43,28 @@ public class ProgramRegister {
             }
             System.out.println("Registration date is invalid.");
         }
-        String parrentPhone = Inputter.getString("Enter parrent phone: ", "This field cannot be empty.");
-        String parrentEmail = Inputter.getString("Enter parrent email: ", "This field cannot be empty.");
+        String parentPhone = Inputter.getString("Enter parent phone: ", "This field cannot be empty.");
+        String parentEmail = Inputter.getString("Enter parent email: ", "This field cannot be empty.");
         String path = "\\src\\registrationform\\" + initFileName(s, p);
-        createForm(path, s, p, parrentPhone, parrentEmail);
+        createForm(path, s, p, regidDate , parrentPhone, parrentEmail);
         return path;
     }
 
-//  Student id: Student name:...
-//  Major: Email:
-//  Address:                  Email:
-    
-    private static void createForm(String path, Student s, Program p, String parrentPhone, String parrentEmail) {
-        File file = new File(Loader.initPath(path)) ;
-        try {
-            file.createNewFile();
-            BufferedWriter output = new BufferedWriter(new FileWriter(file));
-            output.write("Information Student:");
-            String stuLine1 = String.format("Student id: %13s Student name: %s",s.getId() , s.getName());
-            String stuLine2 = String.format("Major: %18s Email: %s    Phone: %s    Passport: %s",s.getMajor() , s.getPhone() , s.getPassport());
-            String stuLine3 = String.format("", );
-            output.newLine();
-            output.close();
-        } catch (IOException e) {
-        }
+    private static void createForm(String path, Student s, Program p, Date regisDate , String parentPhone, String parentEmail) {
+        ArrayList<String> dta = new ArrayList<String>() ;
+        dta.add("                                             Aboard Program Registration Form");
+        dta.add("Information of student: ");
+        dta.add(String.format("Student id: %13s Student name: %s", s.getId(), s.getName()));
+        dta.add(String.format("Major: %18s Email: %s    Phone: %s    Passport: %s", s.getMajor(),
+                s.getPhone(), s.getPassport()));
+        dta.add(String.format("Address: %16s Email of the parent: %s    Phone of the parents: %s",
+                s.getAddress(), parentPhone, parentEmail));
+        dta.add("Information of the aboard program:");
+        dta.add(String.format("Program id: %13s Program name: %s", p.getId() , p.getName()));
+        dta.add(String.format("Time: %19s Days: %d    Location: %s    Cost: %.2f$",p.getTime() , p.getLocation(),p.getDays(),p.getCost())) ;
+        dta.add("Information of the registration:");
+        dta.add("Registration date:        %s",DateModifier.toDateString(regisDate, null)) ;
+        Loader.writeToFile(path, dta);
     }
 
     private static String initFileName(Student s, Program p) {
